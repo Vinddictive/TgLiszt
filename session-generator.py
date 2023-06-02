3e4d2a67e70ff8112120f69753b51cf4d74560b5
Doddy <54377513+Vinddictive@users.noreply.github.com>
2023-06-02 12:26:27 +0700

    Update session-generator.py

--- session-generator.py
+++ session-generator.py
@@ -4,7 +4,7 @@ from telethon.sync import TelegramClient
 api_id = 26892726
 api_hash = 'd80afce6c6f8412ce0e9d1ae5a01f8c7'  # Replace with your API hash
 
-with TelegramClient('+6285722891689.session', api_id, api_hash) as client:
+with TelegramClient('my_session.session', api_id, api_hash) as client:
     print("Your session file has been created successfully!")
 
 
@@ -12,10 +12,18 @@ with TelegramClient('+6285722891689.session', api_id, api_hash) as client:
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
 
-# Replace 'api_id' and 'api_hash' with your own values
-api_id = 28369134
-api_hash = 'ee44f7d8d05288b83789dc5df185061d'
+api_id = 26892726
+api_hash = 'd80afce6c6f8412ce0e9d1ae5a01f8c7'
 
 with TelegramClient(StringSession(), api_id, api_hash) as client:
     session_string = client.session.save()
     print(f"Your session string:\n{session_string}")
+
+
+### CREATE SESSION FILE WITH PYROGRAM ###
+from pyrogram import Client
+
+api_id = 26892726
+api_hash = 'd80afce6c6f8412ce0e9d1ae5a01f8c7'
+with Client('my_session', api_id, api_hash) as app:
+    app.send_message('me', 'Hello, Pyrogram!')
