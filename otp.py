2b65a97a633d554912b32ea39ec2d28043436c7c
Doddy <54377513+Vinddictive@users.noreply.github.com>
2023-12-07 21:14:45 +0700

    Update otp.py

--- otp.py
+++ otp.py
@@ -1,4 +1,4 @@
-from telethon.sessions import StringSession  # Function if you want to use session string
+from telethon.sessions import StringSession
 from telethon.sync import TelegramClient, events
 import re
 
@@ -7,7 +7,9 @@ api_hash = 'ee44f7d8d05288b83789dc5df185061d'
 your_session = 'my_session.session'
 
 client = TelegramClient(your_session, api_id, api_hash)
-# client = TelegramClient(StringSession(your_session), api_id, api_hash) # Use this if you want to use session string
+
+# Use this if you want to use session string
+# client = TelegramClient(StringSession(your_session), api_id, api_hash)
 
 @client.on(events.NewMessage(from_users=777000))
 async def handle_incoming_message(event):
