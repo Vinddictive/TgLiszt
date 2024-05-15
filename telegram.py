483e1c29f7779e9a966afcc96049e5cd45d00a61
Ken <54377513+Vinddictive@users.noreply.github.com>
2024-05-15 14:00:57 +0700

    Update telegram.py

--- telegram.py
+++ telegram.py
@@ -1,151 +1,237 @@
-from telethon.sync import TelegramClient, events
-from telethon.sessions import StringSession
-from telethon.tl.types import Channel
-from pyrogram import Client
-import pyperclip
-
+import sqlite3
 import json
 import csv
+import sys
 import re
 
+try:
+    import pyperclip
+except ModuleNotFoundError:
+    print("Error: Pyperclip library is not installed. Please install it using 'pip install pyperclip'")
+    sys.exit()
 
-class TelethonSession:
+try:
+    from telethon.sync import TelegramClient, events
+    from telethon.sessions import StringSession
+    from telethon.tl.types import Channel
+    from telethon import functions
+    import telethon.errors
+except ModuleNotFoundError:
+    print("Error: Telethon library is not installed. Please install it using 'pip install telethon'")
+    sys.exit()
+
+try:
+    from pyrogram import Client, filters
+except ModuleNotFoundError:
+    print("Error: Pyrogram library is not installed. Please install it using 'pip install pyrogram'")
+    sys.exit()
+
+
+class SessionManager:
     @staticmethod
-    def session_file(api_id: int, api_hash: str, phone, password=None):
+    def telethon(session_file: bool = False, session_string: bool = False) -> None:
         """
-        Create new telethon session file.
-        :param api_id: Your api id.
-        :param api_hash: Your api hash.
-        :param phone: Your phone number.
-        :param password: Optional if the account have 2FA enabled.
+        Create new Telethon sessions.
+        :param session_file: Create Telethon session file.
+        :param session_string: Generate Telethon string session.
         """
-        client = TelegramClient(f'{phone}.session', api_id, api_hash)
-        client.start(phone, password)
-        print("\n🟢 Success!")
+        if session_file:
+            api_id_sf = int(input("Enter your API ID: "))
+            api_hash_sf = input("Enter your API HASH: ")
+            phone = input("Enter your phone number in international format: ")
+            password = input("Enter 2-Step Verification: ")
+            client1 = TelegramClient(f'{phone}.session', api_id_sf, api_hash_sf)
+            client1.start(phone, password)
+            print("\n   🟢 Success!")
+
+        elif session_string:
+            print("\n   1. Create string session by logging in\n   2. Generate string session from a session file\n")
+            session_method = input("Choose how you want to create the session string (Type 1 or 2): ")
+
+            if session_method == "1":
+                api_id_ssl = int(input("\nEnter your API ID: "))
+                api_hash_ssl = str(input("Enter your API hash: "))
+                phone_ssl = input("Enter your phone number in international format: ")
+                password_ssl = input("Enter 2-Step Verification: ")
+                with TelegramClient(StringSession(), api_id_ssl, api_hash_ssl).start(phone=phone_ssl, password=password_ssl) as client_ssl:
+                    session_string_ssl = client_ssl.session.save()
+                    pyperclip.copy(session_string_ssl)  # Auto copy the string to Clipboard
+                    print(f"\n{session_string_ssl}\n")
+                    print(f"    🟢 Success!\n"
+                          f"     The session string has been copied to the clipboard.")
+
+            elif session_method == "2":
+                api_id = int(input("\nEnter your API ID: "))
+                api_hash = input("Enter your API hash: ")
+                session_name = input("Enter your Telethon session file name: ")
+                try:
+                    client = TelegramClient(session_name, api_id, api_hash)
+                    string = StringSession.save(client.session)
+                    pyperclip.copy(string)
+                    print(f"\n{string}\n")
+                    print(f"    🟢 Success!\n"
+                          f"     The session string has been copied to the clipboard.")
+                except sqlite3.OperationalError:
+                    print("\nUnable to generate the session string. Please ensure you are using a Telethon session file.")
+            else:
+                print("Invalid input.")
+        else:
+            print("Invalid input.")
 
     @staticmethod
-    def session_string(api_id: int, api_hash: str):
+    def pyrogram(session_file: bool = False, session_string: bool = False) -> None:
         """
-        Get telethon string session.
-        May not work if your account have 2FA enabled. Please disable 2FA to use this method.
-        :param api_id:  Your api id.
-        :param api_hash: Your api hash.
+        Create new Pyrogram sessions.
+        :param session_file: Create Pyrogram session file.
+        :param session_string: Generate Pyrogram string session.
         """
-        with TelegramClient(StringSession(), api_id, api_hash) as client:
-            session_string = client.session.save()
-            pyperclip.copy(session_string)  # Auto copy the string to Clipboard
-            print(f"Your session string:\n{session_string}\n\n🟢 Success!")
+        if session_file:
+            api_id = int(input("Enter your API ID: "))
+            api_hash = input("Enter your API HASH: ")
+            phone = input("Enter your phone number in international format: ")
+            with Client(phone, api_id, api_hash, phone_number=phone) as client:
+                client.send_message('me', 'Hi!')
+                print("\n   🟢 Success!")
 
+        elif session_string:
+            print("\n   1. Create string session by logging in\n   2. Generate string session from a session file\n")
+            session_method = input("Choose how you want to create the session string (Type 1 or 2): ")
 
-class PyrogramSession:
-    @staticmethod
-    def session_file(api_id: int, api_hash: str, phone: str):
-        """
-        Create new pyrogram session file.
-        :param api_id: Your api id.
-        :param api_hash: Your api hash.
-        :param phone: Your phone number.
-        """
-        with Client(phone, api_id, api_hash, phone_number=phone) as client:
-            client.send_message('me', 'Hello, Pyrogram!')
-            print("🟢 Success!")
-
-    @staticmethod
-    def session_string(api_id: int, api_hash: str, phone: str):
-        """
-        Get pyrogram string session.
-        :param api_id:  Your api id.
-        :param api_hash: Your api hash.
-        :param phone: Your phone number.
-        """
-        with Client(phone, api_id=api_id, api_hash=api_hash, phone_number=phone) as client:
-            session_string = client.export_session_string()
-            pyperclip.copy(session_string)
-            print(f"{session_string}\n\n🟢 Success!")
+            if session_method == "1":
+                api_id = int(input("\nEnter your API ID: "))
+                api_hash = input("Enter your API HASH: ")
+                phone = input("Enter your phone number in international format: ")
+                with Client(phone, api_id, api_hash, phone_number=phone) as client:
+                    pg_string = client.export_session_string()
+                    pyperclip.copy(pg_string)
+                    print(f"\n{pg_string}\n")
+                    print(f"    🟢 Success!\n"
+                          f"     The session string has been copied to the clipboard.")
+            elif session_method == "2":
+                try:
+                    api_id = int(input("\nEnter your API ID: "))
+                    api_hash = input("Enter your API HASH: ")
+                    name = input("Enter your Pyrogram session file name: ")
+                    with Client(name, api_id, api_hash) as client:
+                        pg_string = client.export_session_string()
+                        pyperclip.copy(pg_string)
+                        print(f"\n{pg_string}\n")
+                        print(f"    🟢 Success!\n"
+                              f"     The session string has been copied to the clipboard.")
+                except sqlite3.OperationalError:
+                    print("\nUnable to generate the session string. Please ensure you are using a Pyrogram session file.")
+            else:
+                print("Invalid input.")
+        else:
+            print("Invalid input.")
 
 
 class Telegram:
     @staticmethod
-    def login(session: str, api_id: int, api_hash: str):
+    def login():
         """
-        Login to Telegram using either session file or string.
-        :param session: Path to session file.
-        :param api_id: Your api id.
-        :param api_hash: Your api hash.
+        Login to Telegram using Telethon session file.
         """
-        # The session name must include with its .session format
-        client = TelegramClient(session, api_id, api_hash)
-        client.connect()
-        if client.is_user_authorized():
-            print("🟢 User Authorized")
+        api_id = int(input("Enter your API ID: "))
+        api_hash = input("Enter your API HASH: ")
+        session_name = input("Enter your Telethon session file name: ")
+        try:
+            client = TelegramClient(session_name, api_id, api_hash)
+            client.connect()
+            if client.is_user_authorized():
+                print("\n🟢 User Authorized")
 
-            @client.on(events.NewMessage(from_users=777000))  # '777000' is the ID of Telegram Notification Service.
-            async def handle_incoming_message(event):
-                otp = re.search(r'\b(\d{5})\b', event.raw_text)
-                if otp:
-                    print("OTP received ✅\nYour login code:", otp.group(0))
-                    client.disconnect()
-            print("Please login to your telegram app. [Listening for OTP...]\n")
-            with client:
-                client.run_until_disconnected()
-        else:
-            print("🔴 Authorization Failed\n"
-                  "Incorrect session file or the session no longer active. Please login and try again.")
+                @client.on(events.NewMessage(from_users=777000))  # '777000' is the ID of Telegram Notification Service.
+                async def handle_incoming_message(event):
+                    otp = re.search(r'\b(\d{5})\b', event.raw_text)
+                    if otp:
+                        print("OTP received ✅\nYour login code:", otp.group(0))
+                        client.disconnect()
+                print("Please login to your telegram app. [Listening for OTP...]\n")
+                with client:
+                    client.run_until_disconnected()
+            else:
+                print("\n🔴 Authorization Failed\n"
+                      "Invalid Telethon session file or the session has expired.")
+        except sqlite3.OperationalError:
+            print("\nUnable to generate the session string. Please ensure you are using a Telethon session file.")
 
     @staticmethod
-    def get_group_channel(api_id: int, api_hash: str, session_name: str):
+    def userinfo():
         """
-        Retrieve details from all your own groups and channels, including both public and private.
-        :param api_id: Your api id.
-        :param api_hash: Your api hash.
-        :param session_name: Session file name.
+        Retrieves information about the current user. (Only support Telethon session file.)
         """
-        with TelegramClient(session_name, api_id, api_hash) as client:  # Session name must include its .session format
-            pub_gr = 0
-            priv_gr = 0
-            pub_ch = 0
-            priv_ch = 0
+        try:
+            api_id = int(input("Enter your API ID: "))
+            api_hash = input("Enter your API HASH: ")
+            session_name = input("Enter your Telethon session file name: ")
+            with TelegramClient(session_name, api_id, api_hash) as client:
+                me = client.get_me()
 
-            dialogs = client.get_dialogs()
-            created_groups = [dialog for dialog in dialogs if
-                              isinstance(dialog.entity, Channel) and dialog.entity.creator]
+                name = me.first_name if me.first_name else "-"
+                username = f'@{me.username}' if me.username else "-"
+                user_id = me.id
+                phone_number = me.phone
+                # You can add more details as needed
+                print(
+                    f"\n  [ACCOUNT's INFO]\n\n"
+                    f"  Name: {name}\n"
+                    f"  Username: {username}\n"
+                    f"  ID: {user_id}\n"
+                    f"  Phone Number: +{phone_number}\n\n"
+                    f"1. View all connected devices.\n2. See a list of groups and channels.\n3. Exit.\n"
+                    # Add more fields here if needed
+                )
+                input_user = input("Choose an option by typing its number: ")
+                if input_user == "1":
+                    result = client(functions.account.GetAuthorizationsRequest())
+                    print(result.stringify())
 
-            for group in created_groups:
-                print('Group Name:', group.entity.title)
-                print('Group ID:', group.entity.id)
-                print('Username:', group.entity.username) if group.entity.username else print("Private")
-                print('Creation Date:', group.entity.date.strftime('%Y-%m-%d'))
-                print('---')
+                elif input_user == "2":
+                    pub_gr = 0
+                    priv_gr = 0
+                    pub_ch = 0
+                    priv_ch = 0
 
-                if group.entity.megagroup:
-                    if group.entity.username:
-                        pub_ch += 1
-                    else:
-                        priv_ch += 1
+                    dialogs = client.get_dialogs()
+                    created_groups = [dialog for dialog in dialogs if
+                                      isinstance(dialog.entity, Channel) and dialog.entity.creator]
+
+                    for group in created_groups:
+                        print('\nGroup Name:', group.entity.title)
+                        print('Group ID:', group.entity.id)
+                        print('Username:', group.entity.username) if group.entity.username else print("Username: [Private]")
+                        print('Creation Date:', group.entity.date.strftime('%Y-%m-%d'))
+                        print(
+                            f'Link: https://www.t.me/{group.entity.username}\n' if group.entity.username else 'Link: [Private]')
+
+                        if group.entity.megagroup:
+                            if group.entity.username:
+                                pub_ch += 1
+                            else:
+                                priv_ch += 1
+                        else:
+                            if group.entity.username:
+                                pub_gr += 1
+                            else:
+                                priv_gr += 1
+
+                    print(
+                        f"Public Groups: {pub_gr}\n"
+                        f"Private Groups: {priv_gr}\n"
+                        f"Public Channels: {pub_ch}\n"
+                        f"Private Channels: {priv_ch}\n\n"
+                    )
+
                 else:
-                    if group.entity.username:
-                        pub_gr += 1
-                    else:
-                        priv_gr += 1
-
-            print(
-                f"""
-            Pub Group: {pub_gr}
-            Priv Group: {priv_gr}
-
-            Pub Channel: {pub_ch}
-            Priv Channel: {priv_ch}
-                """
-            )
+                    sys.exit()
+        except sqlite3.OperationalError:
+            print("\nUnable to connect. Please ensure you are using a Telethon session file.")
 
     @staticmethod
-    def member_scrape(api_id: int, api_hash: str, session_name: str, group: str):
+    def member_scrape():
         """
-        Scrape member's info from specified chat (group).
-        :param api_id: Your api id.
-        :param api_hash: Your api hash.
-        :param session_name: Name of your session file.
-        :param group: Target group username (with @)
+        Scrape member's info from specified group. (Only support Telethon session file.)
         """
         def json_save():
             user_info_json = []
@@ -188,6 +274,10 @@ class Telegram:
 
             print("User information saved to 'user_info.csv' file.")
 
+        api_id = int(input("Enter your API ID: "))
+        api_hash = input("Enter your API HASH: ")
+        session_name = input("Enter your Telethon session file name: ")
+        group = input("Enter target group username (with @): ")
         with TelegramClient(session_name, api_id, api_hash) as client:
             user_entity = client.get_me()
             user_username = user_entity.username if user_entity.username else user_entity.id
@@ -235,49 +325,5 @@ class Telegram:
                         continue
             except ValueError:
                 print("The group username does not exist or invalid.")
-
-    @staticmethod
-    def session_check(session_type, session, api_id, api_hash):
-        """
-        Simple code to check if a user is authorized.
-        :param session_type: The type of session.
-        :param session: Session information (file path or string session).
-        :param api_id: The Telegram API ID.
-        :param api_hash: The Telegram API hash.
-        """
-        if session_type == "file":
-            client = TelegramClient(session, api_id, api_hash)
-            client.connect()
-            if client.is_user_authorized():
-                print("User is authorized ✅")
-            else:
-                print("User is not authorized ❌")
-        elif session_type == "string":
-            client = TelegramClient(StringSession(session), api_id, api_hash)
-            client.connect()
-            if client.is_user_authorized():
-                print("User is authorized ✅")
-            else:
-                print("User is not authorized ❌")
-
-    @staticmethod
-    def send_message(session_type, session, username, msg, api_id, api_hash):
-        if session_type == "file":
-            client = TelegramClient(session, api_id, api_hash)
-
-            async def send_message():
-                chat = await client.get_entity(username)
-                await client.send_message(chat, msg)
-                print(f'Message sent to {username} ✅')
-            with client:
-                client.loop.run_until_complete(send_message())
-
-        elif session_type == "string":
-            client = TelegramClient(StringSession(session), api_id, api_hash)
-
-            async def send_message():
-                chat = await client.get_entity(username)
-                await client.send_message(chat, msg)
-                print(f'Message sent to {username}: {msg}')
-            with client:
-                client.loop.run_until_complete(send_message())
+            except telethon.errors.rpcerrorlist.ChatAdminRequiredError as e:
+                print(e)
